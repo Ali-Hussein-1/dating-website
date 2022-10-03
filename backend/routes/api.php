bf0ec036201569cdc8727e0169e868188200e6f4
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-10-03 15:06:22 +0300

    Adding a Route for the signup API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,7 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post("/login", [LandingController::class, "login"])->name("login"); 
+Route::post("/login", [LandingController::class, "login"])->name("login");
+
+Route::post("/signup", [LandingController::class, "signup"])->name("signup"); 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
