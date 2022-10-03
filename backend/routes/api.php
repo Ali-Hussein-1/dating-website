74349db4c8110d654580d2fdf91cb5e0e2de144e
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-10-03 21:12:12 +0300

    Editing the API file

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,10 +4,19 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandingController;
 
-Route::post("/login", [LandingController::class, "login"])->name("login");
+Route::group(["prefix"=>"v1"],function(){
+    
+    Route::group(["middleware"=>"auth:api"],function(){
 
-Route::post("/signup", [LandingController::class, "signup"])->name("signup"); 
+        Route::post("/login", [LandingController::class, "login"])->name("login");
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+        Route::post("/signup", [LandingController::class, "signup"])->name("signup");
+
+    })
+
+
+})
+
+ 
+
+
