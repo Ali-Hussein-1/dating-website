e3d6b7b60486d32d79538af9b5937022ea9c94cb
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-10-03 14:42:55 +0300

    Creating a login API

--- backend/app/Http/Controllers/LandingController.php
+++ backend/app/Http/Controllers/LandingController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class LandingController extends Controller
 {
-    //
+    function login(Request $request){
+        $email = $request->email;
+        $password = $request->password;
+        
+        $user = User::
+                    where("email", $email)
+                    ->where("password",$password)
+                    ->get();
+
+        return response()->json([
+            "status" => "Success",
+            "message" => "Success"
+        ]);
+    }
 }
