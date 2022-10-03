1dcc59a7d2ea8f810b588223a3215d5bceb3efda
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-10-03 14:48:34 +0300

    Adding a Route for the login API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
+Route::post("/login", [LandingController::class, "login"])->name("login"); 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
