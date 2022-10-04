4e049baf3e29eb5d7a81c6227d7c92d160d0661e
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-10-04 22:09:00 +0300

    Linking the signup page

--- frontend/scripts/scripts.js
+++ frontend/scripts/scripts.js
@@ -2,6 +2,13 @@ const signup = document.getElementById("signup");
 const loginEmail = document.getElementById("loginEmail");
 const loginPassword = document.getElementById("loginPassword");
 const signinBtn = document.getElementById("signinBtn");
+const signupEmail = document.getElementById("signupEmail");
+const signupPassword = document.getElementById("signupPassword");
+const signupName = document.getElementById("signupName");
+const signupGender = document.getElementById("signupGender");
+const signupLocation = document.getElementById("signupLocation");
+const signupAge = document.getElementById("signupAge");
+const signupBtn = document.getElementById("signupBtn");
 
 signup.onclick = ()=>{
     window.location.href = "./signup.html"}
@@ -25,3 +32,25 @@ signinBtn.addEventListener('click', () => {
             })
             .catch((err) => console.log(err));
     });
+
+
+    
+    signupBtn.onclick = ()=>{
+        let formdata = new URLSearchParams();
+        formdata.append("email",signupEmail.value);
+        formdata.append("password",signupPassword.value);
+        formdata.append("name",signupName.value);
+        formdata.append("gender",signupGender.value);
+        formdata.append("location",signupLocation.value);
+        formdata.append("age",signupAge.value);
+        formdata.append("bio",'No bio');
+        formdata.append("image",'URL');
+
+        axios
+            .post('http://127.0.0.1:8000/api/v1/signup', formdata)
+                .then((res) => {
+                    console.log(res.data.data[0]);
+                    })
+                }
+
+        
