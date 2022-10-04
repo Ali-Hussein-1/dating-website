201bcef7be0553f6c35f7d4518f84428c9a1ac41
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-10-04 11:03:40 +0300

    Adding a showMales API

--- backend/app/Http/Controllers/HomeController.php
+++ backend/app/Http/Controllers/HomeController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class HomeController extends Controller
 {
-    //
+    function showMales(){
+        $males = User::
+                    where('gender','male')
+                    ->get();
+        return response()->json([
+            "status" => "Success",
+            "data" => $males
+        ]);
+
+    }
 }
