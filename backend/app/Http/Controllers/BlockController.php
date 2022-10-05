43ea7dd03b55526c0698fdf6697f3d077e1ec7cd
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-10-05 14:19:47 +0300

    Creating an addBlock API

--- backend/app/Http/Controllers/BlockController.php
+++ backend/app/Http/Controllers/BlockController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Block;
+use App\Models\User;
 
 class BlockController extends Controller
 {
-    //
+    function addBlocks(Request $request){
+        DB::table('blocks')->insert(
+            ['blocker_id' => $request->blocker_id ,
+             'blocked_id' => $request->blocked_id]
+        ); 
+
+
+        return response()->json([
+            "status" => "Success",
+        ]);
+    }
+
+
 }
