41808bf6eca698ffd862eb643035365cdae63433
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-10-05 12:23:29 +0300

    Creating an addFavorite API

--- backend/app/Http/Controllers/FavouritesController.php
+++ backend/app/Http/Controllers/FavouritesController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class FavouritesController extends Controller
 {
-    
+    function addFavorite(Request $request){
+        $favorite = Favorite::create([
+            'id_1'=>$request->id_1,
+            'id_2'=>$request->id_2,
+        ]) ;
+
+        $favorite->save();
+
+        return response()->json([
+            "status" => "Success",
+            "message" => $favorite
+        ]);
+    }
 }
