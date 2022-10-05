649cbfbd4936e39f4f89c5b29504efc9c0f1bc55
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-10-05 12:17:47 +0300

    Editing the chats table

--- backend/database/migrations/2022_10_04_210139_create_chats_table.php
+++ backend/database/migrations/2022_10_04_210139_create_chats_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('chats', function (Blueprint $table) {
             $table->id();
+            $table->text("text");
             $table->integer("user_id")->references('id')->on("users");
             $table->integer("user_id_1")->references('id')->on("users");
             $table->timestamps();
